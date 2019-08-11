71ebb340da1969a1fbbe8daf17241c7ab8a24f3e
Krzychu Sieradzki <krzysztofsieradzki00@gmail.com>
2019-08-11 12:29:37 +0200

    Gen

---
+++ src/main/java/GenericPairExcercise/MainPair.java
@@ -0,0 +1,13 @@
+package GenericPairExcercise;
+
+public class MainPair {
+
+    public static void main(String[] args) {
+
+        Pair<Integer,String> pair = new Pair(2, "dwa");
+        Pair<String,String> pair2 = new Pair(2, 3);
+
+        String string = pair.getValue();
+
+    }
+}
